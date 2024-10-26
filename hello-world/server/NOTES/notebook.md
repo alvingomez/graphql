0b41e4f44db79608125edb3d978c26054ee4c4af
Alvin Gomez <devvingomez@gmail.com>
2024-10-26 20:12:53 +0800

    Boiler plate
    Created boiler plate for the graphql Server

---
+++ hello-world/server/NOTES/notebook.md
@@ -0,0 +1,32 @@
+1.)Apollo server
+   --allows us to EXPOSE the API over HTTP
+
+2.)Grapql package
+   --provides the core graphql functionality
+
+
+GrapQL 
+  --has by default "schema polling" enabled which means it keeps requesting the schema from the server.
+    to make sure that it is using the latest version. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hello-world/server/server.js
@@ -0,0 +1,32 @@
+// This will contain the server funtionality
+import {ApolloServer} from '@apollo/server';
+
+// Use this to start the Apollo server on its own without using any backend frameworks
+import {startStandaloneServer} from '@apollo/server/standalone';
+
+// SCHEMA DEFINITION LANGUAGE
+// graphql
+const typeDefs = `
+#This is the default but we can leave it out.
+schema{
+    query:Query
+}
+
+# The type definitions represent the interface for our API
+# They declare what our clients can request
+    type Query {
+        greeting: String 
+    }      
+`;
+
+// Resolvers are the "implementation" 
+const resolvers = {
+    Query:{
+        greeting: () => 'Hello world!',
+    }
+}
+
+//New Apollo instance
+const server = new ApolloServer({typeDefs, resolvers});
+const {url} = await startStandaloneServer(server, {listen: {port:9000}});
+console.log(`Server running at ${url}`);
